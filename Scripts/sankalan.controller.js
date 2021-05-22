3ab4e091ad7a940519543156301e91736819afd0
Harsh Chiki <harshchiki@gmail.com>
2021-05-22 11:54:38 +0530

    changes for Sankalan view

--- Scripts/sankalan.controller.js
+++ Scripts/sankalan.controller.js
@@ -6,24 +6,44 @@ $(document).ready(function() {
         console.log(width);
         $("div.container").width(width + "px");
 
-        $('#example').DataTable( {
+        $('#sankalanTable').DataTable( {
             data: dataSet,
-            columns: [
-                
-                { title: "Name (English)" },
-                
-                { title: "Book (English)" },
-                { title: "Bachan" },
-                { title: "Shabd" },
 
-                
-                { title: "Occasion (English)" },
+            scrollY:        '60vh',
+            scrollCollapse: true,
+            paging:         false,
+
+
+            columns: [
+                { title: "Name (Hindi)", width:"12%" },
+                { title: "Name (English)", width: "12%" },
+                {title: "Book (Hindi)", width: "12%" },
+                { title: "Book (English)", width: "12%" },
+                { title: "Bachan", width: "50px" },
+                { title: "Shabd", width: "50px"},
+
+                { title: "Occasion (Hindi)", width: "10%" },
+                { title: "Occasion (English)", width: "10%" },
                 { title: "Audio" },
-                { title: "Video" }
+                { title: "Video"}
             ]
         } );
 
         
         $("input[type='search']").width("500px");
-    }, 500);
-} );
+        $("div.dataTables_wrapper").css("margin-left", "1%");
+        $("div.dataTables_wrapper").css("margin-right", "1%");
+        $("div.dataTables_filter").css("float", "left");
+    }, 500);   
+
+
+    // pause all other audios when "this" is played.
+    document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+    }, true);
+});
