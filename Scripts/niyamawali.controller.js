d073e3f4c3d80c78e16b5909a73240b5b51d6aed
Harsh Chiki <chiki@adobe.com>
2024-01-27 09:10:29 +0530

    Changes to add Niyamawali

---
+++ Scripts/niyamawali.controller.js
@@ -0,0 +1,185 @@
+var plotTable = function(idTable, filterFunc) {
+    setTimeout(() => {
+        var width = $("img.HeaderDiv").width();
+        
+        $("div.container").width(width + "px");
+
+        $('#' + idTable).DataTable( {
+            data: dataSet.filter(filterFunc),
+
+            scrollY:        '60vh',
+            scrollCollapse: true,
+            paging:         true,
+            searching:      false,
+            //responsive:     true,
+            pageLength:     6,
+            info:           true, // show how manu entries exist, actually
+            lengthChange:   false, // whether to show the dropdown to switch page size
+
+                
+
+            drawCallback: function() {
+                $("table.audioColumnDataTable tr").css("background-color", "transparent");
+                if($(document).width() < 1000) {
+                    adaptForMobieleView();
+                } else {
+                    adaptForDesktopView();
+                }
+            },
+
+            "aoColumnDefs": [{
+                "sType": "bookname_english",
+                "bSortable": true,
+                "aTargets": [3]
+
+            }],     
+
+            order: [[3, "asc"]],
+
+            columns: [
+                { title: "Name (Hindi)", width: "12%", className: "leftalign" },
+                { title: "Name (English)", width: "14%", className: "leftalign" },
+                {title: "Book (Hindi)",  className: "hideMobileView leftalign", width: "10%" },
+                { title: "Book (English)", width: "11%", className: "leftalign"  },
+                { title: "Bachan", width:"4%", className: "hideMobileView centeralign" },
+                { title: "Shabd", width: "4%", className: "hideMobileView centeralign"},
+
+                { title: "Occasion (Hindi)", width:"10%", className: "hideMobileView", visible: false },
+                { title: "Occasion (English)", width:"10%",  className: "hideMobileView", visible: false },
+                { title: "Audio", width: "15%", className:" centeralign" },
+                { title: "Video", width: "10%", className: "centeralign"}
+            ]
+        } );
+
+        
+        $("div.dataTables_wrapper").css("margin-left", "1%");
+        $("div.dataTables_wrapper").css("margin-right", "1%");
+        $("div.dataTables_filter").css("float", "left");
+    }, 250); 
+}
+
+// Custom Sorting
+jQuery.fn.dataTableExt.oSort["bookname_english-desc"] = function(x, y) {
+    var px = x.trim().toLowerCase();
+    var py = y.trim().toLowerCase();
+
+    var xStart = px.split(' ')[0];
+    var yStart = py.split(' ')[0];
+
+    if(xStart !== yStart) {
+        // x is from Sar Bachan and y Prem Bani
+        // Sar Bachan < Prem Bani (by time)
+        if(xStart === "sar") {
+            return 1;
+        } else {
+            return -1;
+        }
+    } else {
+        var xLast = px.charAt(px.length-1);
+        var yLast = py.charAt(px.length-1);
+
+        if(isNaN(xLast) || isNaN(yLast)) {
+            return -1;
+        }
+
+        if(xLast === yLast) {
+            return 0;
+        }
+        
+        return xLast > yLast ? -1 : 1;
+    }
+};
+
+// Custom Sorting
+jQuery.fn.dataTableExt.oSort["bookname_english-asc"] = function(x, y) {
+    var px = x.trim().toLowerCase();
+    var py = y.trim().toLowerCase();
+
+    var xStart = px.split(' ')[0];
+    var yStart = py.split(' ')[0];
+
+    if(xStart !== yStart) {
+        // x is from Sar Bachan and y Prem Bani
+        // Sar Bachan < Prem Bani (by time)
+        if(xStart === "sar") {
+            return -1;
+        } else {
+            return 1;
+        }
+    } else {
+        var xLast = px.charAt(px.length-1);
+        var yLast = py.charAt(px.length-1);
+
+        if(isNaN(xLast) || isNaN(yLast)) {
+            return 1;
+        }
+        if(xLast === yLast) {
+            return 0;
+        }
+        
+        return xLast < yLast ? -1 : 1;
+    }
+}
+
+ 
+$(document).ready(function() {
+    
+    $("table[shabdByCategory]").each(function(i, tableEl){
+        var tableId = $(tableEl).attr("id");
+        var filterStr = tableIdFilterMap[tableId].trim().toLowerCase();
+        plotTable(tableId, function(el) {
+            var thisCategory = el[7].trim().toLowerCase();
+            if(filterStr === "prayer") {
+                return thisCategory.includes(filterStr)
+                    && !thisCategory.includes("manglacharan and prayer")
+                    && !thisCategory.includes("prayer for daya and mehar");
+            }
+            return thisCategory.includes(filterStr);
+        });
+    });
+
+    // pause all other audios when "this" is played.
+    document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+    }, true); 
+
+});
+
+
+$(window).resize(function() {
+    if($(window).width() < 1000) {
+        adaptForMobieleView();
+    } else {
+        adaptForDesktopView();
+    }
+});
+
+var adaptForMobieleView = function() {
+    $(".hideMobileView").hide();
+    $(".clickMsg").hide();
+}
+
+
+var adaptForDesktopView = function() {
+    $(".hideMobileView").show();
+    $(".clickMsg").show();
+}
+
+var tableIdFilterMap = {
+"Morning" : "After waking up in morning, Niyamawali",
+"RetiringToBed": "Before retiring to bed in evening, Niyamawali",
+"Bhog" : "Bhog",
+"Daily": "Daily",
+"Sunday": "Sunday",
+"Monday": "Monday",
+"Tuesday": "Tuesday",
+"Wednesday": "Wednesday",
+"Thursday": "Thursday",
+"Friday": "Friday",
+"Saturday": "Saturday"
+};
